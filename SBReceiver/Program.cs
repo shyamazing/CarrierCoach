878a120cea7bc0c7884c9e21202bdacc9953d379
shyam-chipsoft <59446981+shyamazing@users.noreply.github.com>
2025-01-18 16:28:58 +0530

    re arrange components

--- SBReceiver/Program.cs
+++ SBReceiver/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace SBReceiver
+﻿using Azure.Messaging.ServiceBus;
+using System.Text;
+
+namespace SBReceiver
 {
     internal class Program
     {
-        static void Main(string[] args)
+        // use azure service bus emulator provided by azure without actual azure subscription
+        // https://github.com/Azure/azure-service-bus-emulator-installer
+        // Connection string to the Azure Service Bus namespace
+        private static string _connectionString = "Endpoint=sb://127.0.0.1;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+
+        // Name of the queue to send messages to
+        private static string _queueName = "queue.1";
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Waiting for messages...");
+
+            // Start receiving messages
+            ReceiveMessage();
+
+            // Wait for user input to exit
+            Console.ReadLine();
+        }
+
+        public static async void ReceiveMessage()
+        {
+            // create bus client
+            ServiceBusClient serviceBusClient = new ServiceBusClient(_connectionString);
+
+            // configure options for receiver to peek lock
+            ServiceBusReceiverOptions options = new ServiceBusReceiverOptions()
+            {
+                ReceiveMode = ServiceBusReceiveMode.PeekLock
+            };
+
+            // create receiver
+            ServiceBusReceiver receiver = serviceBusClient.CreateReceiver(_queueName, options);
+            while (true)
+            {
+                // read message
+                ServiceBusReceivedMessage message = await receiver.ReceiveMessageAsync(TimeSpan.FromSeconds(5));
+                if (message != null)
+                {
+                    // Print received message
+                    Console.WriteLine($"Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+                    // Complete the message so it is not received again
+                    await receiver.CompleteMessageAsync(message);
+                }
+            }
         }
     }
 }
--- SBSender/Program.cs
+++ SBSender/Program.cs
@@ -6,23 +6,33 @@ namespace SBSender
 {
     internal class Program
     {
+        // use azure service bus emulator provided by azure without actual azure subscription
+        // https://github.com/Azure/azure-service-bus-emulator-installer
+        // Connection string to the Azure Service Bus namespace
         private static string _connectionString = "Endpoint=sb://127.0.0.1;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=SAS_KEY_VALUE;UseDevelopmentEmulator=true;";
+
+        // Name of the queue to send messages to
         private static string _queueName = "queue.1";
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Enter message to send. type 'exit' to exit the application");
             bool exit = false;
             do
             {
-                Console.WriteLine("Enter message to send. type 'exit' to exit the application");
+                // Prompt user to enter a message
                 string message = Console.ReadLine()!;
+
+                // Check if the user wants to exit the application
                 if (message.Trim().ToLower() == "exit")
                 {
                     exit = true;
                 }
+                // Check if the message is not empty
                 else if (!string.IsNullOrEmpty(message))
                 {
-                    SendMessage(message);
+                    // Send the message to the queue
+                    await SendMessage(message);
                 }
                 else
                 {
@@ -31,15 +41,28 @@ namespace SBSender
             }
             while (!exit);
         }
-        public static void SendMessage(string message)
+
+        // Method to send a message to the Azure Service Bus queue
+        public static async Task SendMessage(string message)
         {
+            // Create a Service Bus client
             ServiceBusClient serviceBusClient = new ServiceBusClient(_connectionString);
+
+            // Create a sender for the queue
             ServiceBusSender sender = serviceBusClient.CreateSender(_queueName);
+
+            // Serialize the message to JSON format
             string messageBody = JsonSerializer.Serialize(message);
+
+            // Create a Service Bus message with the serialized message body
             ServiceBusMessage messageContent = new ServiceBusMessage(Encoding.UTF8.GetBytes(messageBody));
-            sender.SendMessageAsync(messageContent);
-            sender.DisposeAsync();
-            serviceBusClient.DisposeAsync();
+
+            // Send the message asynchronously
+            await sender.SendMessageAsync(messageContent);
+
+            // Dispose the sender and client asynchronously
+            await sender.DisposeAsync();
+            await serviceBusClient.DisposeAsync();
         }
     }
 }
